d221ac311f91304916966fcee7c12b516748e3ed
FaridAlieff <faridalieff@AlieffsMacBook-Pro.local>
2023-09-13 03:08:27 +0400

    First JS code

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+document.write("Farid Aliyev <br>");
+debugger
+document.write("FrontEnd Development <br>")
+debugger
+document.write("Garage Academy © 2023 <br>")
+debugger
+
+document.getElementById("Hello").innerHTML = "Hallo";
+document.getElementById("World").innerHTML = "Welt";
+document.getElementById("empty").innerHTML = "HTML bos ola biler amma JS doludu 😎"
